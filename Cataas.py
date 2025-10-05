973bdf0a33ce6f8187075ef03f4b3873453670d2
Egor Elesin <egor.elesin@ya.ru>
2025-10-05 21:45:41 +0300

    Создана ф для получения изображения

--- Cataas.py
+++ Cataas.py
@@ -1,9 +1,24 @@
+from http.client import responses
 from tkinter import *
 # from pill
 # from pillow import Image, ImageTk
 # from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+from PIL import ImageTk
+
+
+def load_image:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'Error: {e}')
+        return None
 
 window = Tk()
 window.title('Cats')
