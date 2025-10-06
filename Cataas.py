a4b9e1a3dc3154a50ce220bb949cfb6979d72709
Egor Elesin <egor.elesin@ya.ru>
2025-10-06 09:42:10 +0300

    Залепил ПИЛ

--- Cataas.py
+++ Cataas.py
@@ -5,6 +5,11 @@ from io import BytesIO
 
 from PIL import ImageTk
 
+def set_image():
+    img = load_image(url)
+    if img:
+        label.config(image=img)
+        label.image = img
 
 def load_image(url):
     try:
@@ -12,6 +17,7 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600,480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f'Error: {e}')
@@ -24,13 +30,19 @@ window.geometry('500x500')
 label = Label()
 label.pack()
 
+update_button = Button(text='Update', command=set_image)
+update_button.pack()
+
+
 url = 'https://cataas.com/cat'
 
-img = load_image(url)
+set_image()
 
-if img:
-    label.config(image=img)
-    label.image = img
+# img = load_image(url)
+#
+# if img:
+#     label.config(image=img)
+#     label.image = img
 
 window.mainloop()
 
